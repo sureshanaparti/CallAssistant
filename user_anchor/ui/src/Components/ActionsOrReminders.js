4338e4d2c8c78cd78fe6a4391baaf8ce702a4046
vedulasantosh <vedula.santosh@accelerite.com>
2019-03-09 23:40:20 +0530

    UI: On click events for actions

--- user_anchor/ui/src/Components/ActionsOrReminders.js
+++ user_anchor/ui/src/Components/ActionsOrReminders.js
@@ -1,21 +1,71 @@
 import React from 'react';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
-const ActionsOrReminders = (props) => {
-  const data = props.data.filter(prop => prop.type === 'action');
+class ActionsOrReminders extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div>
-      <h4>Actions</h4>
-      <ListGroup variant='flush'>
-        {data.map((prop, index) => {
-          return (<ListGroup.Item variant="primary" key={`${index}`}>
-            {prop.message}
-          </ListGroup.Item>);
-        })}
-      </ListGroup>
-    </div>
-  );
+    this.handleShow = this.handleShow.bind(this);
+    this.handleClose = this.handleClose.bind(this);
+
+    this.state = {
+      show: false,
+    };
+
+    this.data = props.data;
+  }
+
+  handleClose() {
+    console.log("in close");
+    this.setState({ show: false });
+  }
+
+  handleShow() {
+    console.log("in show");
+    this.setState({ show: true });
+  }
+
+  componentWillReceiveProps() {
+
+  }
+
+  render() {
+    return (
+      <div>
+        <h4>Actions</h4>
+        <ListGroup variant='flush'>
+          {this.props.data.map((prop, index) => {
+            return (<ListGroup.Item variant="primary" key={`${index}`}>
+              {prop.message}
+              <Button variant="primary" size = "sm"  className = "float-right" onClick={this.handleShow}>
+              view
+              </Button>
+              <Modal show={this.state.show} onHide={this.handleClose}>
+                <Modal.Header closeButton>
+                  <Modal.Title>{prop.message}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>{prop.message}</Modal.Body>
+                <Modal.Footer>
+                  <Button variant="secondary" onClick={this.handleClose}>
+                    Close
+                  </Button>
+                  <Button variant="primary" onClick={this.handleClose}>
+                    Save Changes
+                  </Button>
+                </Modal.Footer>
+              </Modal>
+            </ListGroup.Item>);
+          })}
+        </ListGroup>
+      </div>
+    );
+  }
 }
+
+//const ActionsOrReminders = (props) => {
+  //const data = props.data.filter(prop => prop.type === 'action'); 
+//}
 
 export default ActionsOrReminders;
