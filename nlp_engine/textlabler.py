d707e0df5e0791478b9ccd6e82567723d22537a7
Harikrishna Patnala <harikrishna.patnala@accelerite.com>
2019-03-10 03:00:24 +0530

    Date Time converter

--- nlp_engine/textlabler.py
+++ nlp_engine/textlabler.py
@@ -9,6 +9,7 @@ import falcon
 import spacy
 import os
 from notifier import notifier
+import datetime
 
 #os.sys.path.append('/home/semicolon/coref/bin')
 
@@ -36,6 +37,66 @@ class AllResource(object):
         ticketList = self.ticketDictionary['id']
         ticketList.add('CS-' + ticket)
         self.ticketDictionary['id'] = ticketList
+
+    def timeconvert(self, str1):
+
+        list = str1.split()
+        am = ""
+        pm = ""
+        time = ""
+        for item in list:
+            if item == 'a.m.':
+                am = 'am'
+            elif item == 'p.m.':
+                pm = 'pm'
+            elif item[0].isdigit() == True:
+                time = item
+
+        str1 = time + " " + am + pm
+
+        print(str1)
+        n=0
+        while(str1[n].isdigit()!=True):
+            n=n+1
+
+        str1=str1[n:]
+
+        if str1[2:3]!=":":
+            str1="0"+str1
+        if str1[-2:].lower() == "am" and str1[:2] == "12":
+            return "00" + str1[2:-2]
+        elif str1[-2:].lower() == "am":
+            return str1[:-2]
+        elif str1[-2:].lower() == "pm" and str1[:2] == "12":
+            return str1[:-2]
+        else:
+            return str(int(str1[:2]) + 12) + str1[2:5]+":00"
+
+    def find_date(self, in_day):
+        d=datetime.datetime.now()
+        if in_day.lower()=="tomorrow":
+            days_ahead=1
+            return d + datetime.timedelta(days_ahead)
+        if in_day.lower()=="day after tomorrow":
+            days_ahead=2
+            return d + datetime.timedelta(days_ahead)
+        if in_day.lower()=="today":
+            return d
+        switcher = {
+
+            "monday": 0,
+            "tuesday": 1,
+            "wednesday": 2,
+            "thursday":  3,
+            "friday": 4,
+            "saturday": 5,
+        }
+        val=switcher.get(in_day.lower())
+        days_ahead = val - d.weekday()
+        if days_ahead <= 0:
+            days_ahead += 7
+        return d + datetime.timedelta(days_ahead)   #2019-03-15T12:00:00   #2019-03-15T12:00:00
+
 
     def lableText(self, text, jobid):
         self.actionList = []
@@ -124,6 +185,13 @@ class AllResource(object):
                 self.addAttendees(attendee)
             self.meetingDictionary['attendees'] = ",".join(list(self.meetingDictionary['attendees']))
             self.meetingDictionary['jobid'] = jobid
+
+            if self.meetingDictionary['date'] == None:
+                self.meetingDictionary['date'] = 'wednesday'
+            if self.meetingDictionary['time'] == None:
+                self.meetingDictionary['time'] = '3PM'
+
+            self.meetingDictionary['starttime'] = self.find_date(self.meetingDictionary['date']).strftime("%Y-%m-%dT")+self.timeconvert(self.meetingDictionary['time'])
             self.notifier.notify(jobid, "log", "Extracted meeting related params %s" % json.dumps(self.meetingDictionary))
             self.actionList.append(self.meetingDictionary)
 
