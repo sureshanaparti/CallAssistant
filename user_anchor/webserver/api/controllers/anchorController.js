1e4d175ba4222a84e9abfc8666d933ca316e350e
Srinivas Gandikota <srinivas.gandikota@accelerite.com>
2019-03-09 18:18:22 +0530

    User Anchor: Persist all the events in mysql-lite inmemory db

--- user_anchor/webserver/api/controllers/anchorController.js
+++ user_anchor/webserver/api/controllers/anchorController.js
@@ -3,13 +3,20 @@
 
 require('body-parser');
 
+
 //wss = require('server.js').websocketServer;
 //wss = require('server.js').websocketServer;
 exports.notify_event = function(req, res) {
     //Write to DB, send notifications to active websockets
 
     //TODO store in DB
-
+   global.db.run(`INSERT INTO events(event) VALUES(?)`, [JSON.stringify(req.body)], function(err) {
+        if (err) {
+          return console.log(err.message);
+        }
+    // get the last insert id
+        console.log(`A row has been inserted with rowid ${this.lastID}`);
+   });
     //websocketServer
     global.wss.clients.forEach(function each(client) {
       //client.send("broadcast: spanner updated");
@@ -25,3 +32,14 @@ exports.notify_event = function(req, res) {
 exports.invoke_action = function(req, res) {
     res.json('{"invoking":"Action"}')
 };
+
+/*
+db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+});
+
+*/
+
--- user_anchor/webserver/server.js
+++ user_anchor/webserver/server.js
@@ -10,6 +10,21 @@ const serverPort = 3000,
     websocketServer = new WebSocket.Server({ server });
 global.wss = websocketServer;
 
+const sqlite3 = require('sqlite3').verbose();
+
+let db = new sqlite3.Database(':memory:', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+});
+
+db.run('CREATE TABLE events(event text)');
+
+global.db = db;
+
+
+
 var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -20,7 +35,22 @@ routes(app);
 //when a websocket connection is established
 websocketServer.on('connection', (webSocketClient) => {
     //send feedback to the incoming connection
-    webSocketClient.send('{ "connection" : "ok"}');
+    //webSocketClient.send('{ "connection" : "ok"}');
+
+   //send all the events in db
+let sql = `SELECT event FROM events`;
+db.all(sql, [], (err, rows) => {
+  if (err) {
+    throw err;
+  }
+  rows.forEach((row) => {
+    console.log(row);
+    //webSocketClient.send(JSON.stringify(row));
+    webSocketClient.send(row.event);
+    
+  });
+});
+
     
     //when a message is received
     webSocketClient.on('message', (message) => {
