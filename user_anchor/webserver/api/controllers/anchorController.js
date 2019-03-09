e66674248d7933067322fa36b18e83f2fa535d8f
Srinivas Gandikota <srinivas.gandikota@accelerite.com>
2019-03-10 02:59:56 +0530

    User Anchor: Invoke Action implemented for meeting requests

--- user_anchor/webserver/api/controllers/anchorController.js
+++ user_anchor/webserver/api/controllers/anchorController.js
@@ -6,31 +6,84 @@ require('body-parser');
 
 //wss = require('server.js').websocketServer;
 //wss = require('server.js').websocketServer;
-exports.notify_event = function(req, res) {
-    //Write to DB, send notifications to active websockets
 
-    //TODO store in DB
-   global.db.run(`INSERT INTO events(event) VALUES(?)`, [JSON.stringify(req.body)], function(err) {
+var store_event = function(event_info) {
+
+   global.db.run(`INSERT INTO events(event) VALUES(?)`, [event_info], function(err) {
         if (err) {
           return console.log(err.message);
         }
     // get the last insert id
         console.log(`A row has been inserted with rowid ${this.lastID}`);
    });
-    //websocketServer
+
+
+}
+
+var notify_clients = function(event_info) {
+
     global.wss.clients.forEach(function each(client) {
       //client.send("broadcast: spanner updated");
-      client.send(JSON.stringify(req.body));
+      client.send(event_info);
       //client.send("notification:"+JSON.stringify(req.body));
     });
+
+
+}
+
+
+exports.notify_event = function(req, res) {
+    //Write to DB, send notifications to active websockets
+
+    //store in DB
+   store_event(JSON.stringify(req.body));
+
+    //websocketServer
+    notify_clients(JSON.stringify(req.body));
+
     //console.log(req.body);
     res.json('{"status":"Done"}')
 };
 
 
+var request = require('request');
+let anchor_url="http://localhost:3002/createEvent"
+//let anchor_url="http://10.233.88.74:3002/createEvent"
+
 
 exports.invoke_action = function(req, res) {
     res.json('{"invoking":"Action"}')
+
+
+console.log("Invoke Body: ", req.body);
+
+//req.body.type = "action";
+store_event(JSON.stringify(req.body))
+notify_clients(JSON.stringify(req.body));
+
+var req_body = req.body.message;
+var action = req.body.action;
+
+ console.log("Executing Action ", req_body);
+console.log(" Action:: ", req_body.action);
+
+if (action === "meeting") {
+console.log("Calling Meeting handler");
+ request.post(
+     anchor_url,
+     { json: req_body },
+     function (error, response, body) {
+         if (!error && response.statusCode == 200) {
+             console.log(body);
+         }
+     }
+ );
+
+} else if (action === "jiraBug") {
+
+
+}
+
 };
 
 /*
